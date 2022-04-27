f9dd4b20708f23f8e44f86b8e38998124fc9f2ac
jirivinduska <49195922+jirivinduska@users.noreply.github.com>
2022-04-27 16:37:48 +0200

    led count

--- change-color.py
+++ change-color.py
@@ -9,7 +9,7 @@ Session = sqlalchemy.orm.sessionmaker()
 Session.configure(bind=engine)
 session = Session()
 
-LED_COUNT = 150        # Number of LED pixels.
+LED_COUNT = 300        # Number of LED pixels.
 LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
 # LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
